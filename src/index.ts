33389f5d32a549be9427e264f21877b7fd9e8e36
linhuaxin <linhuaxin@foxmail.com>
2019-11-19 01:21:16 +0800

    handle params

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import {RequestConfig} from './types'
+import { RequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function request(config: RequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: RequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: RequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default request
